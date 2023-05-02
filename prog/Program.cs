4792a99140d779a40b9783aac746027cf4bbc08e
Neonn7528 <Neonn7@yandex.ru>
2023-05-02 17:00:57 +0300

    added program folder

---
+++ prog/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string[] firstArray = Console.ReadLine().Split(' ');
+string[] nums = new string[firstArray.Length];
+SecondArray(firstArray, nums);
+PrintArray(nums);
+
+void SecondArray(string[] firstArray, string[] nums)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            nums[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+       Console.WriteLine($"[{string.Join(" ", array)}]");
+}
